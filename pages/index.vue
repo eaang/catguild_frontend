1a7ad79f99b966c2afe367b89bd036c87fd93d43
Evan Ang <alethiologist@gmail.com>
2021-04-02 22:01:08 +0800

    testing without the async

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="px-6 space-y-2">
-    <div class="flex justify-between items-center">
-      <div class="text-2xl font-bold">{{ intro.title }}</div>
-      <div class="w-6 h-6 hover:text-gray-400 cursor-pointer">
-        <PencilAlt />
-      </div>
-    </div>
-    <!-- eslint-disable-next-line vue/no-v-html -->
-    <div class="space-y-2" v-html="$md.render(intro.content)"></div>
+    <div class="flex justify-between items-center">Hello world</div>
   </div>
 </template>
 
@@ -17,9 +10,6 @@ export default {
     return {
       intro: {},
     }
-  },
-  async fetch() {
-    this.intro = await this.$strapi.find('introduction')
   },
 }
 </script>
