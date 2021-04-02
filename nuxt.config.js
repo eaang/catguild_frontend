f766060c5290f6cd394a2857e2ce78b513534e4c
Evan Ang <alethiologist@gmail.com>
2021-04-03 02:33:21 +0800

    trying to swap server

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: 'http://localhost:1337/graphql',
+        httpEndpoint: 'https://catguild.herokuapp.com/graphql',
       },
     },
   },
