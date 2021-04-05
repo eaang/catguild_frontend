8f9b5b1f3f1e5ad28fecdccaf661237e761ba9a4
Evan Ang <alethiologist@gmail.com>
2021-04-05 21:55:59 +0800

    working on sidebar categories

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="">
-    <div class="flex justify-between items-center cursor-pointer">
-      <nuxt-link to="/rules" class="flex-grow">
-        <SidebarMain title="Rules" />
-      </nuxt-link>
-
-      <div class="px-2" @click="dropdown1 = !dropdown1">
-        <div v-if="dropdown1" class="h-4 w-4"><ChevronDown /></div>
-        <div v-if="!dropdown1" class="h-4 w-4"><ChevronUp /></div>
+    <div v-for="category in categories" :key="category.id">
+      <div class="flex justify-between items-center cursor-pointer">
+        <nuxt-link to="/rules" class="flex-grow">
+          <SidebarMain
+            :title="category.name"
+            :class="{ disabled: category.blocks.length === 0 }"
+          />
+        </nuxt-link>
+        <div v-if="category.blocks.length !== 0">
+          <div class="px-2" @click="dropdown1 = !dropdown1">
+            <div v-if="dropdown1" class="h-4 w-4"><ChevronDown /></div>
+            <div v-if="!dropdown1" class="h-4 w-4"><ChevronUp /></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import categoriesQuery from '~/apollo/queries/categories/categories'
+
 export default {
   data() {
     return {
       dropdown1: false,
     }
+  },
+  apollo: {
+    categories: {
+      prefetch: true,
+      query: categoriesQuery,
+    },
   },
 }
 </script>
