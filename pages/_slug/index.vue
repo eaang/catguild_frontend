603fff4198199e070244ff5daa9c2facac8e7070
Evan Ang <alethiologist@gmail.com>
2021-04-06 00:53:14 +0800

    added in editing block

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div v-if="categories" class="px-6">
-    <div class="text-2xl font-bold">
+    <div class="text-2xl font-bold uppercase">
       {{ category.name }}
     </div>
-    <div v-for="block in category.blocks" :key="block.id" class="box-section">
-      <span :id="anchorTag(block.title)"></span>
-      <ContentBox :content="block.content" class="my-4" />
+    <div
+      v-for="block in category.blocks"
+      :key="block.id"
+      class="box-section w-full flex"
+    >
+      <!-- Normal Block Text -->
+      <div v-if="editingBlock !== block.id">
+        <span :id="anchorTag(block.title)"></span>
+        <ContentBox :content="block.content" class="my-4" />
+      </div>
+      <!-- Editing Block Text -->
+      <div v-else><ContentEditor :content="content" class="my-4" /></div>
+      <!-- Toggle Options -->
+      <div
+        v-if="editingBlock !== block.id"
+        @click="selectBlock(block.id, block.content)"
+      >
+        <div class="px-2 mt-4">
+          <div class="h-8 w-6 flex items-center cursor-pointer">
+            <Edit />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +33,22 @@
 <script>
 import categoryQuery from '~/apollo/queries/categories/category'
 export default {
+  data() {
+    return {
+      editingBlock: null,
+      content: '',
+    }
+  },
+  computed: {
+    category() {
+      return this.categories[0]
+    },
+  },
+  created() {
+    this.$nuxt.$on('update-content', (e) => {
+      this.updateContent(e)
+    })
+  },
   apollo: {
     categories: {
       prefetch: true,
@@ -22,14 +58,17 @@ export default {
       },
     },
   },
-  computed: {
-    category() {
-      return this.categories[0]
-    },
-  },
   methods: {
     anchorTag(str) {
       return str.match(/\w/g).join('').toLowerCase()
+    },
+    selectBlock(id, content) {
+      this.editingBlock = id
+      this.content = content
+    },
+    updateContent(content) {
+      console.log(content)
+      this.editingBlock = null
     },
   },
 }
